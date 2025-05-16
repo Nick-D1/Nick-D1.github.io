f22daa4d3b205d86700139c65ecbb53376f3ab7b
Nicholas Davis <nwdavis2014@gmail.com>
2025-05-16 13:35:51 -0500

    Module 2 completed baseline

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,9 @@
+
+/* GET travlr view*/
+const travel = (req, res) => {
+    res.render('travel', {title: 'Travlr Getaways'});
+};
+
+module.exports = {
+    travel
+};
---
+++ app_server/routes/travel.js
@@ -0,0 +1,9 @@
+
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/travel');
+
+/*Get travel page*/
+router.get('/', controller.travel);
+
+module.exports = router;
