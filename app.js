64b954931b5d960c7af08ecadcc250bbec148746
Nick-D1 <126429727+Nick-D1@users.noreply.github.com>
2025-10-24 13:54:37 -0500

    Refactor feedbackRouter placement in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var apiRouter = require('./app_api/routes/index'); // API router
-var feedbackRouter = require('./app_server/routes/feedback'); // ✅ keep require here
+var feedbackRouter = require('./app_server/routes/feedback'); // 
 
 const passport = require('passport');
 var handlebars = require('hbs');
@@ -18,7 +18,7 @@ var handlebars = require('hbs');
 require('./app_api/models/db');
 require('./app_api/config/passport');
 
-var app = express();  // ✅ app must exist before app.use()
+var app = express();  // 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -40,12 +40,12 @@ app.use('/api', (req, res, next) => {
   next();
 });
 
-// ✅ wire up routes AFTER app exists
+// Wire up routes AFTER app exists
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter); 
-app.use('/feedback', feedbackRouter); // ✅ moved down here
+app.use('/feedback', feedbackRouter); // 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
